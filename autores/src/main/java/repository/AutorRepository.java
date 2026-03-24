fcc300ded8fc449ffac07775f0264e705d8d453d
unknown <ericleite@hotmail.com>
2026-03-24 20:03:47 -0300

    Segundo commit repository criado

---
+++ autores/src/main/java/repository/AutorRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+}
